5934537fb72a84900afed5dd8bc55530780b88d9
daw2-Covacho22 <covachosantiagosara@fpllefia.com>
2022-05-14 21:38:32 +0200

    Controladores usuarios preparados

--- src/rutas/r_usuarios.js
+++ src/rutas/r_usuarios.js
@@ -1,12 +1,21 @@
 const express = require ('express')
+const {leerUsuarios, leerUsuarioId, crearUsuario, editarUsuarioId, 
+borrarUsuarioId} = require('../controladores/c_usuarios.js')
 const r_usuarios = express.Router()
 
-r_usuarios.get('/', (req, res)=>{
-    res.send('Estás en la ruta de usuarios')
-})
+//- get a /usuarios -->leerUsuarios()
+r_usuarios.get('/', leerUsuarios)
 
-r_usuarios.get('/web', (req, res)=>{
-    res.send('Estás en la ruta de usuarios web')
-})
+//- get a /usuarios/:id -->leerUsuarioId()
+r_usuarios.get('/:id', leerUsuarioId)
+
+//- post a /usuarios -->crearUsuario()
+r_usuarios.post('/', crearUsuario)
+
+//- put a /usuarios/:id -->editarUsuarioId()
+r_usuarios.put('/:id', editarUsuarioId)
+
+//- delete a /usuarios/:id -->borrarUsuarioId()
+r_usuarios.delete('/:id', borrarUsuarioId)
 
 module.exports = r_usuarios
